6a87246c11f6538fe630ddc24ce84157caeffb73
Amar Meddahi <71496992+amarmeddahi@users.noreply.github.com>
2022-12-29 12:45:48 +0100

    phong model

--- src/main.m
+++ src/main.m
@@ -2,15 +2,72 @@ clear;
 close all;
 
 % Load images and lights
-load('data_tp5/eclairages.mat')
-load('data_tp5/donnees.mat')
+load('data.mat', 'data')
+load('calib.mat', 'calib')
 
-% Convert the ligh directions from (phi,theta) to (x,y,z)
-[x,y,z] = sph2cart(pi/2 - phi, pi/2 - theta, ones(size(phi)));
-calib.S = [x y z];
+% Load robust_PS data
+load('N.mat','N')
+load('rho_d.mat','rho_d')
+load('S.mat','S')
 
-% Images
-data.I = images;
+% Phong model
+v = [0 0 -1]; % view direction
 
-% Robust PS
-[~,N,rho_d,~,S,~,~,~] = robust_ps_V2(data,calib);
+% Specular directions
+normalVectors = reshape(N, [], 3);
+numLights = size(S,1);
+numPixels = size(normalVectors,1);
+specularDirections = zeros([numLights numPixels 3]);
+lightDirections = S;
+rho_d = rho_d(:);
+% Loop over each light
+for i = 1:numLights
+   specularDirections(i,:,:) = 2 * sum(lightDirections(i, :) .* normalVectors,2) .* (normalVectors -  lightDirections(i, :));
+end
+
+% Build the system Ax = b
+A = zeros([numLights numPixels 2]);
+b = zeros([numLights numPixels]);
+
+for i = 1:numLights
+    A(i,:,1) = 1;
+    A(i,:,2) = log(abs(sum(v .* squeeze(specularDirections(i,:,:)),2))); % add abs (mistake?)
+    b(i,:) = log(abs(reshape(data.I(:,:,i),[],1) - rho_d .* sum(lightDirections(i, :) .* normalVectors,2)));
+end
+
+% Solving
+x = zeros([numLights numPixels 2]);
+for i = 1:numLights
+    A_i = squeeze(A(i,:,:));
+    b_i = b(i,:);
+    for j = 1:numPixels
+        x(i,j,:) = A_i(j,:)\b_i(j);
+    end
+end
+
+% Robust estimation of the specular parameters
+specularParameters = squeeze(median(x,1));
+
+% Parameters
+[r, c, ~] = size(data.I);
+albdeo_diff = rho_d(:);
+albdeo_spec = exp(specularParameters(:,1));
+coeff_spec = specularParameters(:,2);
+
+% Results
+I_ref = data.I(:,:,1);
+I_diff = reshape(albdeo_diff .* sum(lightDirections(1, :) .* normalVectors,2), [r c]);
+I_Phong = I_diff + reshape(albdeo_spec .* (abs(sum(v .* squeeze(specularDirections(1,:,:)),2))).^coeff_spec , [r c]);
+
+figure;
+subplot(1,3,1);
+imshow(I_ref);
+title('Reference Image', 'FontSize', 14, 'FontWeight', 'bold', 'Color', 'red');
+subplot(1,3,2);
+imshow(I_diff);
+title('Model Diffuse (standard method)', 'FontSize', 14, 'FontWeight', 'bold', 'Color', 'red');
+subplot(1,3,3);
+imshow(I_Phong);
+title('Phong Model (ours)', 'FontSize', 14, 'FontWeight', 'bold', 'Color', 'red');
+
+
